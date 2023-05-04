070ae2e3f3a359710bc44169e6ec53bca5693f98
crx <crx@MATEBOOK>
2023-05-04 13:41:45 +0800

    添加项目文件

---
+++ Groupwork/Person.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include <vector>
+using namespace std;
+
+class Person
+{
+private:
+	string name, email, passport, mobile;
+	int id;
+	//
+};
+
