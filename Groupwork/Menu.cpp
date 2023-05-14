b3bb9c464b1de29eee4420faf52444bb808b96c8
blingbling26 <133120940+blingbling26@users.noreply.github.com>
2023-05-14 13:22:25 +0800

    Add files via upload

---
+++ Groupwork/Menu.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Menu {
+public:
+    void SignUp() {
+        
+    }
+
+    void ClassChoosen() {
+
+    }
+
+    void UserType() {
+
+    }
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
