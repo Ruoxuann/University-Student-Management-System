e538e912eebd233e8f89a8fb0dd0645a9fe2b792
huaiqqia <105117182+huaiqqia@users.noreply.github.com>
2023-05-14 19:48:14 +0800

    Update main.cpp
    加了头文件

--- Groupwork/main.cpp
+++ Groupwork/main.cpp
@@ -1,9 +1,17 @@
+#include "Person.h" //0514 1947hqy加的
+#include "Student.h" //0514 1947加
+#include "Course.h"  //0514 1947加
+
 #include<iostream>
 using namespace std;
 
 int main() {
 
 
+	
+	
+	
+	
 
 	return 0;
 }
